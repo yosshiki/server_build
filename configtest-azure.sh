126479db16b0c7fb840eb65eeefd19f30b1a79e6
yosshiki <38740134+yosshiki@users.noreply.github.com>
2024-03-12 23:33:14 +0900

    Create azure-configtest.sh

---
+++ configtest-azure.sh
@@ -0,0 +1,29 @@
+#####################################################################
+# subscription check script
+# prerequisite 
+# - azure login
+# - run in target subscription
+#####################################################################
+!#/bin/bash
+TIME=`date +%Y%m%d%H%M`
+CATEGORY=(subscription resource id_role network webapp data compute monitor)
+OUTPUT_DIR=./azure_configtest_result_$TIME
+
+NUMSEC=0
+NUMLINE=0
+[ ! -e $OUTPUT_DIR ]; mkdir $OUTPUT_DIR
+SUB=`az account show -o tsv --query name`
+
+for i in  ${!CATEGORY[@]}
+do
+  exec <./$i.txt
+  while read line;
+    do
+        echo "############### $SUB $NUMSEC-$NUMLINE.[$line] ################" | tee -a  $OUTPUT_DIR/`eval echo ${CATEGORY[$i]}_result.txt`;
+        sleep 1
+        eval "$line" |tee -a $OUTPUT_DIR/${CATEGORY[$i]}_result.txt
+        NUMLINE=$(($NUMLINE + 1))
+    done
+  NUMLINE=1
+  NUMSEC=$(($NUMSEC + 1))
+done
